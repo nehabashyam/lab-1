1acb323bb41f070d888e05a58001c1d8370fe7aa
rishabhmthakur2 <rishabhmthakur@outlook.com>
2022-01-28 00:37:06 -0800

    Lab 1 problem set

---
+++ lab1.py
@@ -0,0 +1,30 @@
+"""
+Lab 1: Problem Statement
+
+Write a Python function to print all numbers from 1 to 100(inclusive).
+For numbers that are divisible by 3, you should print "Fizz" instead of the number.
+For numbers that are divisible by 5, you should print "Buzz" instead of the number.
+For numbers that are divisible by both 3 and 5, you should print "FizzBuzz" instead of the number.
+
+Sample I/O:
+
+Input: None
+
+Output:
+1
+2
+Fizz
+4
+Buzz
+...
+14
+FizzBuzz
+16
+...
+"""
+
+def printNumbers():
+    # Enter your code after this line. Replace the pass statement.
+    pass
+
+printNumbers()
